1a18e8e51ba0ac47de20d2719fd4cf6d46e225aa
sudarshan <sudarshanprasad18@gmail.com>
2024-01-03 14:21:35 +0530

    feat: Updating the product

--- database.js
+++ database.js
@@ -18,7 +18,7 @@ export async function uploadItems(
 ) {
     const insert = await
         pool.query(
-            'INSERT INTO plush.items (name, image, price1, price2, price3, discountPrice, weight1,weight2,weight3, ingredients, howtouse, benefits, category, availability) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? ,?)',
+            'INSERT INTO plush.items (name, image, price1, price2, price3, discountPrice, weight1,weight2,weight3, ingredients, howtouse, benefits, category, availability) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? ,?,?,?,?,?)',
             [name, image, price1, price2, price3, discount, weight1, weight2, weight3, ingredients, howtouse, benefits, category, availability]
         )
 }
@@ -26,5 +26,12 @@ export async function uploadItems(
 export async function getProducrs(category) {
     const query = await
         pool.query(`SELECT * FROM plush.items WHERE category like '${category}'`)
+    return query[0];
+}
+
+export async function editProduct(id, name, image, price1, price2, price3, discount, weight1, weight2, weight3, ingredients, howtouse, benefits, category, availability
+) {
+    const query = await
+        pool.query(`UPDATE plush.items SET name = '${name}', image = '${image}', price1 = '${price1}', price2 = '${price2}', price3 = '${price3}', discountPrice = '${discount}', weight1 = '${weight1}', weight2 = '${weight2}', weight3 = '${weight3}', ingredients = '${ingredients}', howtouse = '${howtouse}', benefits = '${benefits}', category = '${category}', availability = '${availability}' WHERE id = '${id}'`)
     return query[0];
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // Purpose: Entry point for the application
 import express from 'express'
-import { uploadItems, getProducrs } from '../database.js'
+import { uploadItems, getProducrs,editProduct } from '../database.js'
 import path from 'path'
 import multer from 'multer'
 
 const app = express();
 app.use(express.static('uploads'));
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.static('uploads'));
 
 const __dirname = path.resolve();
@@ -48,6 +48,12 @@ app.get('/product', async (req, res) => {
    res.status(200).send({
       "items": items
    });
+});
+
+app.patch('/updateProduct', async (req, res) => {
+   const { id, name, image, price1, price2, price3, discountPrice, weight1, weight2, weight3, ingredients, howtouse, benefits, category, availability } = req.body;
+   const items = await editProduct(id, name, image, price1, price2, price3, discountPrice, weight1, weight2, weight3, ingredients, howtouse, benefits, category, availability);
+   res.status(200).send("success");
 });
 
 const port = process.env.PORT || 3000
